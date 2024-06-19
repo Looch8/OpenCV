7f9eed7056c592a342ac1f06b7a7adf753a27e4f
Luke Wheldale <lukewheldale@gmail.com>
2024-06-19 14:37:34 +0930

    new image

--- detectCells.py
+++ detectCells.py
@@ -2,22 +2,19 @@ import cv2 as cv
 import numpy as np
 
 # Load the image
-img = cv.imread('Photos/PICT0010.jpeg')
+img = cv.imread('Photos/PICT0001.jpeg')
 
 # Convert to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 # Apply blur to reduce noise and improve blob detection
-blurred = cv.GaussianBlur(gray, (7, 7), 0)
+blurred = cv.GaussianBlur(gray, (5, 5), 0)
 
-# Apply Otsu's thresholding to obtain a binary image
-_, thresh = cv.threshold(blurred, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-
-# Invert the binary image
-thresh = 255 - thresh
+# Apply binary thresholding to obtain a binary image
+_, thresh = cv.threshold(blurred, 100, 255, cv.THRESH_BINARY)
 
 # Apply morphological operations to enhance blob detection
-kernel = np.ones((5, 5), np.uint8)
+kernel = np.ones((3, 3), np.uint8)
 cleaned = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel)
 
 # Set up the SimpleBlobDetector parameters
@@ -26,19 +23,19 @@ params = cv.SimpleBlobDetector_Params()
 # Filter by Area
 params.filterByArea = True
 params.minArea = 100  # Adjust minArea as needed
-params.maxArea = 10000  # Adjust maxArea as needed
+params.maxArea = 3000  # Adjust maxArea as needed
 
 # Filter by Circularity
 params.filterByCircularity = True
-params.minCircularity = 0.1
+params.minCircularity = 0.5  # Adjust minCircularity as needed
 
 # Filter by Convexity
 params.filterByConvexity = True
-params.minConvexity = 0.87
+params.minConvexity = 0.8  # Adjust minConvexity as needed
 
 # Filter by Inertia
 params.filterByInertia = True
-params.minInertiaRatio = 0.01
+params.minInertiaRatio = 0.1  # Adjust minInertiaRatio as needed
 
 # Create a detector with the parameters
 detector = cv.SimpleBlobDetector_create(params)
@@ -81,10 +78,10 @@ print(f'Number of blobs detected: {num_blobs}')
 
 # Write the number of blobs on the image with red text at the top middle
 text = f'Cells detected: {num_blobs}'
-text_size, _ = cv.getTextSize(text, cv.FONT_HERSHEY_SIMPLEX, 2, 3)
+text_size, _ = cv.getTextSize(text, cv.FONT_HERSHEY_SIMPLEX, 1, 2)
 text_x = (result.shape[1] - text_size[0]) // 2
 cv.putText(result, text, (text_x, 50),
-           cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+           cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
 # Show the image with blobs detected and the number of blobs
 cv.imshow('Blobs', result)
